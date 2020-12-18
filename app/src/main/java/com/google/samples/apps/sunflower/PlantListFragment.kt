e0ecb47c702b06b77aafbc939f4697c55672b114
yue <1263887632@qq.com>
2020-12-18 17:23:56 +0800

    format

--- app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
@@ -17,12 +17,7 @@
 package com.google.samples.apps.sunflower
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.samples.apps.sunflower.adapters.PlantAdapter
@@ -36,9 +31,9 @@ class PlantListFragment : Fragment() {
     private val viewModel: PlantListViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentPlantListBinding.inflate(inflater, container, false)
         context ?: return binding.root
